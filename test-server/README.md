88baf1595fb9fdf08221a4c88a849644a1dabe38
Chunkai Yang <mattyang@fb.com>
2015-10-19 22:11:20 -0700

    removing a wasted dic

--- test-server/README.md
+++
@@ -1,2 +0,0 @@
-# Multiwii-raspberry-drone-server
-Python code to control Multiwii powered quadcopter by raspberry pi
--- test-server/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# My first script
-
-sudo hostapd -B /etc/hostapd/hostapd.conf
-sudo ifconfig wlan0 192.168.10.1
-sudo service isc-dhcp-server restart
